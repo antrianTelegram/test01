afb172b1f6ddcf4dc8faffa32cf6a58069b7ffdc
gsukra <widyawan37@gmail.com>
2016-12-29 21:29:39 +0800

    second updated

---
+++ runAntrian.py
@@ -0,0 +1,12 @@
+import os
+import sys
+import time
+
+os.system ("ps -elf | grep 'python antrian01.py' | awk '{print $4}' | xargs sudo kill")
+os.system ("ps -elf | grep 'python /home/pi/dataku/antrian/antrian01.py' | awk '{print $4}' | xargs sudo kill")
+
+#os.system ("ps -elf | grep 'python buttonmatrix2.py' | awk '{print $4}' | xargs sudo kill")
+#os.system ("ps -elf | grep 'python /home/pi/dataku/antrian/data_lama/buttonmatrix2.py' | awk '{print $4}' | xargs sudo kill")
+
+#os.system ("python /home/pi/dataku/antrian/data_lama/buttonmatrix2.py")
+os.system ("python /home/pi/dataku/antrian/antrian01.py")
